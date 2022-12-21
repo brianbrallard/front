2ccca707a14f1cb4be4f6728f9906c8416b4130d
Brian <polobrallardbrian@gmail.com>
2022-12-21 00:15:10 -0300

    finished passwordsgenerator

--- PasswordGenerator/app.js
+++ PasswordGenerator/app.js
@@ -1,2 +1,17 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+let pass1El = document.getElementById('pass1')
+let pass2El = document.getElementById('pass2')
+
+function generate(){
+    for (let i = 0; i<15;i++){
+        let randomNumbers = Math.floor(Math.random()*characters.length);
+        pass1El.textContent += characters[randomNumbers]}
+    for (let i = 0; i<15;i++){
+         let randomNumbers = Math.floor(Math.random()*characters.length);
+         pass2El.textContent += characters[randomNumbers]
+}setTimeout(function(){
+    location.reload(true);
+},5000);
+
+}
